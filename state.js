eb302c8c60d4ba412a793c7c7f1dc5de8a9a753e
simoneevacca <v.simone.vacca@gmail.com>
2024-04-08 11:46:06 +0200

    add club array in state.js

---
+++ state.js
@@ -0,0 +1,44 @@
+import {reactive} from 'vue'
+
+export const state = reactive({
+
+    clubArray: [
+        {
+            name: 'Istanbul Sports',
+            logo: 'league-club-1.png'
+        },
+        {
+            name: 'United Fs Club',
+            logo: 'league-club-2.png'
+        },
+        {
+            name: 'Schalke Club',
+            logo: 'league-club-3.png'
+        },
+        {
+            name: 'Inter Club',
+            logo: 'league-club-4.png'
+        },
+        {
+            name: 'Madrid Club',
+            logo: 'league-club-5.png'
+        },
+        {
+            name: 'Schalke Club',
+            logo: 'league-club-6.png'
+        },
+        {
+            name: 'Arsenal Club',
+            logo: 'league-club-7.png'
+        },
+        {
+            name: 'Bayern Club',
+            logo: 'league-club-8.png'
+        },
+        {
+            name: 'Atletico Club',
+            logo: 'league-club-9.png'
+        },
+    ]
+
+}) 
