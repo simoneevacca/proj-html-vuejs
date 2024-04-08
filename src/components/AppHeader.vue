5c6774d399c6c906ad83a4eb314a423618fcad0d
simoneevacca <v.simone.vacca@gmail.com>
2024-04-08 13:52:31 +0200

    add style to slider

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -43,11 +43,11 @@
 
         <div class="slider" v-for="(image, index) in slider">
             <img :class="isVisible(index)" :src="slider[index]" alt="">
-        </div>
-        <div class="controller">
-            <div @click="nextImage" class="next-button"> next </div>
-            <div @click="prevImage" class="prev-button"> prev </div>
-
+            <div class="controller">
+                <div :class="isVisible(index)" @click="prevImage" class="prev-button"><i class="fa-solid fa-chevron-left"></i></div>
+                <div :class="isVisible(index)" @click="nextImage" class="next-button"><i class="fa-solid fa-chevron-right"></i></div>
+    
+            </div>
         </div>
 
     </header>
@@ -58,10 +58,53 @@
 
     header {
 
+        & .slider{
+            position: relative;
+            & img.visible {
+                width: 100%;
+                /* height: 600px; */
+                object-fit: cover;
 
-        & img:not(.visible) {
-            display: none;
+            }
+
+
+            & img:not(.visible) {
+                display: none;
+            }
+
+            & .controller {
+                width: 100%;
+                position: absolute;
+                top: 50%;
+                transform: translate(0, -50%);
+                display: flex;
+                justify-content: space-between;
+                color: white;
+
+                & div:not(.visible){
+                    display: none;
+                }
+
+                & .prev-button, .next-button {
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    width: 6rem;
+                    height: 6rem;
+                    font-size: 2rem;
+                    padding: 2rem;
+                    margin: 3rem;
+                    border: 3px solid white;
+                    
+                    border-radius: 50%;
+                    
+                    
+                }
+    
+            }
         }
+
+
     }
 
 </style>
