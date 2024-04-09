df2a55d6931fed5497c386690ab3ade26b61bc9b
simoneevacca <v.simone.vacca@gmail.com>
2024-04-09 10:36:12 +0200

    add logic to players section

--- src/components/Players.vue
+++ src/components/Players.vue
@@ -6,42 +6,65 @@ export default {
         return {
             playersArray: [
                 {
+                    id: 1,
                     image: '/images/player1.jpg',
                     number: '07',
                     name: 'Adam Brown',
                     role: 'Forwarder'
                 },
                 {
+                    id: 2,
                     image: '/images/player2.jpg',
                     number: '09',
                     name: 'Michael Kayn',
                     role: 'Forwarder'
                 },
                 {
+                    id: 3,
                     image: '/images/player3.jpg',
                     number: '11',
                     name: 'Michael Lee',
                     role: 'Forwarder'
                 },
                 {
+                    id: 4,
                     image: '/images/player4.jpg',
                     number: '15',
                     name: 'Ethan Smith',
                     role: 'Forwarder'
                 },
                 {
+                    id: 5,
                     image: '/images/player5.jpg',
                     number: '20',
                     name: 'Brian Hooks',
                     role: 'Forwarder'
                 },
                 {
+                    id: 6,
                     image: '/images/player6.jpg',
                     number: '18',
                     name: 'Stan Jackson',
                     role: 'Forwarder'
                 },
-            ]
+            ],
+            viewAll: false,
+            button: 'View All Players'
+        }
+
+    },
+    methods: {
+        showAll() {
+            this.viewAll = !this.viewAll
+            if (this.button == 'View All Players') {
+                this.button = 'Hide Players'
+
+            } else if (this.button == 'Hide Players') {
+                this.button = 'View All Players'
+
+            }
+
+            console.log(this.viewAll);
         }
     }
 }
@@ -49,8 +72,23 @@ export default {
 
 <template>
 
-    <div class="container" >
-        <div class="row">
+    <div class="container">
+
+        <div class="row" v-if="viewAll=== false">
+            <div class="col" v-for="index in 4" :key="index">
+                <div class="card">
+                    <img :src="playersArray[index - 1].image" alt="">
+                    <div class="text">
+                        <div class="number">{{ playersArray[index - 1].number }}</div>
+                        <div>{{ playersArray[index - 1].name }} - {{ playersArray[index - 1].role }}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="row" v-else-if="viewAll === true">
             <div class="col" v-for="player in playersArray">
                 <div class="card">
                     <img :src="player.image" alt="">
@@ -59,8 +97,18 @@ export default {
                         <div>{{ player.name }} - {{ player.role }}</div>
                     </div>
                 </div>
+
             </div>
         </div>
+        <!-- <div v-else-if="viewAll === true">
+            <div v-for="index in 4" :key="index">
+                {{ playersArray[index - 1].name }}
+            </div>
+        
+        </div> -->
+        
+        <button @click="showAll()">{{ button }}</button>
+
 
     </div>
 
@@ -68,50 +116,53 @@ export default {
 </template>
 
 <style scoped>
-    .container {
-        max-width: 95%;
-        margin: auto;
-        
-        & .row {
-            display: flex;
-            flex-wrap: wrap;
-            
-                    
+.container {
+    max-width: 95%;
+    margin: auto;
 
-            & .col {
-                width: calc((100% / 12) * 3);
-                padding: 2rem;
-                 & .card {
-                     position: relative;
-                    
-                    & img {
-                        width: 100%;
-                        border-radius: 50px;
-                        
+    & .row {
+        display: flex;
+        flex-wrap: wrap;
 
+
+
+        & .col {
+            width: calc((100% / 12) * 3);
+            padding: 2rem;
+
+            & .card {
+                position: relative;
+
+                & img {
+                    width: 100%;
+                    border-radius: 50px;
+
+
+                }
+
+                & .text {
+                    position: absolute;
+                    bottom: 0px;
+                    z-index: 1;
+                    background-color: black;
+                    color: white;
+                    width: 100%;
+                    border-radius: 30px;
+                    height: 25%;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
+
+                    & .number {
+                        font-size: 2rem;
+                        font-weight: bold
                     }
-                    & .text {
-                        position: absolute;
-                        bottom: 0px;
-                        z-index: 1;
-                        background-color: black;
-                        color: white;
-                        width: 100%;
-                        border-radius: 30px;
-                        height: 25%;
-                        display: flex;
-                        flex-direction: column;
-                        justify-content: center;
-                        align-items: center;
-                        & .number {
-                            font-size: 2rem;
-                            font-weight: bold
-                        }
-                    }
+                }
 
-                 }
-                
             }
+
         }
     }
+}
 </style>
